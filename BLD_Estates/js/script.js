51b9c26ec36183b2dc5f7f0e508511b97b8b1b00
namle <nduy.nghia.w@gmail.com>
2024-10-11 06:45:45 +0700

    Huge HomePage Update

--- BLD_Estates/js/script.js
+++ BLD_Estates/js/script.js
@@ -1,33 +1,25 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the Home button
     const homeButton = document.querySelector(".navigation a[href='#home']");
-    // Select the About button
     const aboutButton = document.querySelector(".navigation a[href='#about']");
 
-    // Add a click event listener to the Home button
     homeButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default action if it's an anchor
-        // Redirect to the home page (you can replace the URL with your actual home page URL)
-        window.location.href = "home.html"; // Assuming your home page is named index.html
+        event.preventDefault();
+        window.location.href = "home.html"; 
     });
 
-    // Add a click event listener to the About button
     aboutButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default action
-        // Redirect to the about page (replace with your actual About page URL)
-        window.location.href = "about.html"; // Assuming your about page is named about.html
+        event.preventDefault(); // 
+        window.location.href = "about.html"; 
     });
 });
-
-
-
 
 
 
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnLogin = document.querySelector('.btnLog');
+const btnLogin = document.querySelectorAll('.btnLog');
 const iconclose = document.querySelector('.icon-close')
 
 registerLink.addEventListener('click', ()=> {
@@ -37,10 +29,23 @@ registerLink.addEventListener('click', ()=> {
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
-
-btnLogin.addEventListener('click', ()=> {
+function showLoginPopup() {
     wrapper.classList.add('active-Log');
+}
+btnLogin.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = "home.html";
+        sessionStorage.setItem('showLogin', 'true');
+      
+    });
 });
+if (sessionStorage.getItem('showLogin') === 'true') {
+    sessionStorage.removeItem('showLogin');
+    setTimeout(() => {
+        showLoginPopup();
+    }, 500); 
+}
 
 iconclose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-Log');
