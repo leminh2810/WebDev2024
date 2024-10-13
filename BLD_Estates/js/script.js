b936a652931f3116dbc9e358a2b652e13a9d91c6
namle <nduy.nghia.w@gmail.com>
2024-10-13 20:57:37 +0700

    Button Update, Header optimization, CSS omision

--- BLD_Estates/js/script.js
+++ BLD_Estates/js/script.js
@@ -1,52 +1,35 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-    const homeButton = document.querySelector(".navigation a[href='#home']");
-    const aboutButton = document.querySelector(".navigation a[href='#about']");
-
-    homeButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        window.location.href = "home.html"; 
-    });
-
-    aboutButton.addEventListener("click", function(event) {
-        event.preventDefault(); // 
-        window.location.href = "about.html"; 
-    });
+    const homeButton = document.querySelector(".navigation a[href='home.html']");
+    const aboutButton = document.querySelector(".navigation a[href='about.html']");
+    const BuyPage = document.querySelector(".navigation a[href='buy_sell.html']");
+    const SellPage = document.querySelector(".navigation a[href='buy_sell.html']");
+    if (homeButton) {
+        homeButton.addEventListener("click", function(event) {
+            console.log("Home button clicked");
+        });
+    }
+    if (aboutButton) {
+        aboutButton.addEventListener("click", function(event) {
+            console.log("About button clicked");
+        });
+    }
+    if (BuyPage) {
+        aboutButton.addEventListener("click", function(event) {
+            console.log("Buy Page clicked");
+        });
+    }
+    if (SellPage) {
+        aboutButton.addEventListener("click", function(event) {
+            console.log("Buy Page clicked");
+        });
+    }
 });
 
-
-
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnLogin = document.querySelectorAll('.btnLog');
-const iconclose = document.querySelector('.icon-close')
-
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
+document.getElementById("BuyPage").addEventListener("click", function() {
+    window.location.href = "buy_sell.html";
 });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
+document.getElementById("SellPage").addEventListener("click", function() {
+    window.location.href = "buy_sell.html";
 });
-function showLoginPopup() {
-    wrapper.classList.add('active-Log');
-}
-btnLogin.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        event.preventDefault();
-        window.location.href = "home.html";
-        sessionStorage.setItem('showLogin', 'true');
-      
-    });
-});
-if (sessionStorage.getItem('showLogin') === 'true') {
-    sessionStorage.removeItem('showLogin');
-    setTimeout(() => {
-        showLoginPopup();
-    }, 500); 
-}
 
-iconclose.addEventListener('click', ()=>{
-    wrapper.classList.remove('active-Log');
-})
